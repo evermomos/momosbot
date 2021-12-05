6eca7b7fa23267d24eedfe6a347b15017c6eb15a
evermomos <shigezibot18@hotmail.com>
2021-12-04 20:18:07 -0600

    th

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: 'help',
     execute(message){
-        message.channel.send({ content: 'La lista de comandos disponibles son: !waif, !nekos, !manynekos, !thegame, !olliemine, !help, !ping, !bingchilling, '})
+        message.channel.send({ content: 'La lista de comandos disponibles son: !waif (sfw or nsfw) (category), !nekos, !manynekos (sfw or nsfw) (category), !thegame, !olliemine, !help, !ping, !bingchilling, Puedes ver las categorias en: https://waifu.pics/more '})
     }
 }
