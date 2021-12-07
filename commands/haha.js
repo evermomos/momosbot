2bb860dfeb09531ddb559d62dc77b74eb9ce8765
evermomos <shigezibot18@hotmail.com>
2021-12-06 23:21:55 -0600

    gopi dog command hahbal

---
+++ commands/haha.js
@@ -0,0 +1,6 @@
+module.exports = {
+    name: 'dogpi',
+    execute(message){
+        message.channel.send({ content: 'https://cdn.discordapp.com/emojis/917644799941693542.png?size=128'})
+    }
+}
