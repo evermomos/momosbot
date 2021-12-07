8d2503f4a5adff8c916eb5f7d81551e3c94fd1fb
evermomos <shigezibot18@hotmail.com>
2021-12-07 03:45:38 -0600

    added command and updated !help xd stfu olemin

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: 'help',
     execute(message){
-        message.channel.send({ content: 'La lista de comandos disponibles son: !waif (sfw or nsfw) (category), !nekos, !manynekos (sfw or nsfw) (category), !thegame, !olliemine, !hexai, !help, !ping, !bingchilling, Puedes ver las categorias en: https://waifu.pics/more '})
+        message.channel.send({ content: 'La lista de comandos disponibles son: !waif (sfw or nsfw) (category), !nekos, !manynekos (sfw or nsfw) (category), !thegame, !olliemine, !hexai, !help, !ping, !bingchilling, !dogpi, !pssssssss (!ps) Puedes ver las categorias en: https://waifu.pics/more '})
     }
 }
