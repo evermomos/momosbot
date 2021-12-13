d7406ee39b75f30d6abec61891ef9169ff9b273c
YTOliver <73561517+YTOliver@users.noreply.github.com>
2021-12-13 10:27:22 -0600

    Fixed alliases and added a check for bad server response

--- commands/waif.js
+++ commands/waif.js
@@ -3,9 +3,10 @@ module.exports = {
     name: 'waif',
     async execute(message, args){
         fetch(`https://api.waifu.pics/${args[0]}/${args[1]}`)
-        .then(res => res.json())
-        .then(body => {
-            if(!body.url) return message.channel.send({ content: "Invalid arguments, valid arguments are: <https://waifu.pics/more>"})
+        .then(async res => {
+			if(res.status !== 200) return message.channel.send({ content: `Error ${res.status} ${res.statusText}` });
+			const body = await res.json()
+			if(!body.url) return message.channel.send({ content: "Invalid arguments, valid arguments are: <https://waifu.pics/more>"})
             return message.channel.send({content: body.url })
     
         })
