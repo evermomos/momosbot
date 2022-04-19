f17b05428574e99a0090c18ded3186af8b5b2b40
evermomos <shigezibot18@hotmail.com>
2022-04-19 01:12:54 -0500

    pls dont judge 3 commit in a single day

--- commands/addserver.js
+++ commands/addserver.js
@@ -1,20 +1,14 @@
 const { MessageEmbed } = require("discord.js")
-const { OSChannel } = require("../info.json")
-const { client } = require("../index")
 
 module.exports = {
-	name : "addserver",
-	description: "yes",
-	admin: true,
-	dm: true,
-	cooldown: -1,
-	async execute(message, args) {
+    name : "addserver",
+    async execute(message, args, client) {
         const discordlink = args.shift()
         const color = args.shift()
         const name = args.join(" ")
         const channel = client.channels.cache.get('965847850602881025')
         const inviteinf = await client.fetchInvite(discordlink)
-		const embed = new MessageEmbed()
+        const embed = new MessageEmbed()
         .setTitle(name)
         .setThumbnail(inviteinf.guild.iconURL())
         .setColor(color)
